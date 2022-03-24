cff1c65270e681e63bc7d5f4e58017c348eac14e
nurahasny <nura.alhasani@gmail.com>
2022-03-24 21:32:34 +0700

    menambahkan contoh1.php,welcome.php,index.html

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h2>PERKENALKAN</h2>";
+        echo "Nama saya Nura isa alhasny<br>
+        Saya tinggal di daerah Cengkareng olahraga
+        yang saya sukai adalah Bulutangkis";
+    }
+}
